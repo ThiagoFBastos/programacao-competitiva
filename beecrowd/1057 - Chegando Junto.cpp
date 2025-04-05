adf616875bf53a5a67d73c0adee0d65f8f5e5d5a
Thiago Bastos <thiagobastos1705@gmail.com>
2025-04-05 10:38:20 -0300

    fix: 1057 - Chegando Junto

--- beecrowd/1057 - Chegando Junto.cpp
+++ beecrowd/1057 - Chegando Junto.cpp
@@ -1,131 +1,256 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
-#include <tuple>
-#include <utility>
-#include <cassert>
+#include <bits/stdc++.h>
 
-#define INDICE(x, y, columns) ((x) * columns + (y))
+using namespace std;
 
-constexpr int N = 100;
+struct point {
 
-const std::pair<int, int> deltas[] = {
-    {0, 1},
-    {0, -1},
-    {1, 0},
-    {-1, 0}
+	int x0, x1;
+	int y0, y1;
+	int z0, z1;
 };
 
-int dist[N][N][N];
-
 int main() {
-
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-
-    int t;
-
-    std::cin >> t;
-
-    for(int c = 1; c <= t; ++c) {
-        int n;
-
-        std::cin >> n;
-
-        std::vector<std::string> labirinto(n);
-        std::vector<std::pair<int, int>> posicoes;
-        std::queue<std::tuple<int, int, int, int, int, int>> queue;
-
-        for(int k = 0; k < n; ++k) {
-            std::cin >> labirinto[k];
-
-            for(int j = 0; j < n; ++j) {
-                if(labirinto[k][j] >= 'A' && labirinto[k][j] <= 'C')
-                    posicoes.emplace_back(k, j);
-            }
-        }
-
-        for(int i = 0; i < n * n; ++i)
-            for(int j = 0; j < n * n; ++j)
-                for(int k = 0; k < n * n; ++k)
-                    dist[i][j][k] = -1;
-
-        for(int i = 0; i < n * n; ++i) {
-            for(int j = 0; j < n * n; ++j) {
-                dist[i][i][j] = 0;
-                dist[i][j][i] = 0;
-                dist[j][i][i] = 0;
-                dist[i][i][i] = 0;
-            }
-        }
-
-        assert(posicoes.size() == 3);
-
-        auto [x0, y0] = posicoes[0];
-        auto [x1, y1] = posicoes[1];
-        auto [x2, y2] = posicoes[2];
-
-        dist[INDICE(x0, y0, n)][INDICE(x1, y1, n)][INDICE(x2, y2, n)] = 0;
-
-        queue.emplace(x0, y0, x1, y1, x2, y2);
-
-        auto posicaoValida = [&labirinto, n](int x, int y)->bool {
-            return !(x < 0 || y < 0 || x >= n || y >= n || labirinto[x][y] == '#');
-        };
-
-        auto combinacaoValida = [&labirinto, n, &posicaoValida, &queue](int x0, int y0, int x1, int y1, int x2, int y2, int dx, int dy)->void {
-             int distAtual = dist[INDICE(x0, y0, n)][INDICE(x1, y1, n)][INDICE(x2, y2, n)];
-
-            x0 += dx, y0 += dy;
-            x1 += dx, y1 += dy;
-            x2 += dx, y2 += dy;
-
-            if(!posicaoValida(x0, y0)) x0 -= dx, y0 -= dy;
-            if(!posicaoValida(x1, y1)) x1 -= dx, y1 -= dy;
-            if(!posicaoValida(x2, y2)) x2 -= dx, y2 -= dy;
-
-            if(x0 == x1 && y0 == y1 || x0 == x2 && y0 == y2) x0 -= dx, y0 -= dy;
-            if(x0 == x1 && y0 == y1 || x1 == x2 && y1 == y2) x1 -= dx, y1 -= dy;
-            if(x2 == x1 && y2 == y1 || x0 == x2 && y0 == y2) x2 -= dx, y2 -= dy;
-            
-            int& distNova = dist[INDICE(x0, y0, n)][INDICE(x1, y1, n)][INDICE(x2, y2, n)];
-
-            if(distNova != -1)
-                return;
-
-            distNova = 1 + distAtual;
-            queue.emplace(x0, y0, x1, y1, x2, y2);
-        };
-
-        auto combinacaoDesbloqueada = [&labirinto, n](int x0, int y0, int x1, int y1, int x2, int y2)->bool {
-            return labirinto[x0][y0] == 'X' && labirinto[x1][y1] == 'X' && labirinto[x2][y2] == 'X' && !(x0 == x1 && y0 == y1 || x0 == x2 && y0 == y2 && x1 == x2 && y1 == y2);
-        };
-
-        std::cout << "Case " << c << ": ";
-
-        bool trapped = true;
-        
-        while(!queue.empty()) {
-            auto [x0, y0, x1, y1, x2, y2] = queue.front();
-
-            queue.pop();
-
-             int distAtual = dist[INDICE(x0, y0, n)][INDICE(x1, y1, n)][INDICE(x2, y2, n)];
-
-            if(combinacaoDesbloqueada(x0, y0, x1, y1, x2, y2)) {
-                std::cout << distAtual << '\n';
-                trapped = false;
-                break;
-            }
-
-            for(auto [dx, dy] : deltas)
-                combinacaoValida(x0, y0, x1, y1, x2, y2, dx, dy);
-        }
-
-        if(trapped)
-            std::cout << "trapped\n";
-    }
-
-    return 0;
+	
+	int tests;
+	
+	scanf("%d", &tests);
+	
+	for(int i = 1; i <= tests; ++i) {
+	
+		char **grid;
+		int ***state, n, N, x0, x1, y0, y1, z0, z1, target[3][2], h = 0;
+		
+		scanf("%d", &n);
+		
+		N = n * n;
+		
+		grid = new char*[n];
+		state = new int**[N];
+		
+		for(int i = 0; i < n; ++i) {
+		
+			grid[i] = new char[n + 1];
+			
+			scanf("%s", grid[i]);
+			
+			for(int j = 0; j < n; ++j) {
+				
+				switch(grid[i][j]) {
+				
+					case 'A': x0 = i, x1 = j; break;
+					case 'B': y0 = i, y1 = j; break;
+					case 'C': z0 = i, z1 = j; break;
+					case 'X':
+					
+						target[h][0] = i;
+						target[h++][1] = j;
+				}
+			}
+		}
+		
+		for(int i = 0; i < N; ++i) {
+		
+			state[i] = new int*[N];
+			
+			for(int j = 0; j < N; ++j) {
+			
+				state[i][j] = new int[N];
+				memset(state[i][j], -1, N * sizeof(int));
+			}
+		}
+		
+		queue<point> Q;
+		
+		state[x0 * n + x1][y0 * n + y1][z0 * n + z1] = 0;
+		
+		Q.push({x0, x1, y0, y1, z0, z1});
+		
+		while(!Q.empty()) {
+		
+			auto [x0, x1, y0, y1, z0, z1] = Q.front();
+			
+			Q.pop();
+			
+			{
+				int x = x0, y = y0, z = z0;
+				
+				if(x0 < n - 1 && grid[x0 + 1][x1] != '#') ++x;
+				if(y0 < n - 1 && grid[y0 + 1][y1] != '#') ++y;
+				if(z0 < n - 1 && grid[z0 + 1][z1] != '#') ++z;
+				
+				for(int k = 0; k < 2; ++k) {
+				
+					if(x == z && x1 == z1) {
+					
+						if(x0 < z0) x = x0;
+						else z = z0;
+					}
+					
+					if(x == y && x1 == y1) {
+					
+						if(x0 < y0) x = x0;
+						else z = y0;
+					}
+					
+					if(y == z && y1 == z1) {
+					
+						if(y0 < z0) y = y0;
+						else z = z0;
+					}
+				}
+				
+				if(state[x * n + x1][y * n + y1][z * n + z1] == -1) {
+				
+					state[x * n + x1][y * n + y1][z * n + z1] = 1 + state[x0 * n + x1][y0 * n + y1][z0 * n + z1];
+					Q.push({x, x1, y, y1, z, z1});
+				}
+			}
+			
+			{
+				int x = x0, y = y0, z = z0;
+				
+				if(x0 && grid[x0 - 1][x1] != '#') --x;
+				if(y0 && grid[y0 - 1][y1] != '#') --y;
+				if(z0 && grid[z0 - 1][z1] != '#') --z;
+				
+				for(int k = 0; k < 2; ++k) {
+				
+					if(x == z && x1 == z1) {
+					
+						if(x0 > z0) x = x0;
+						else z = z0;
+					}
+					
+					if(x == y && x1 == y1) {
+					
+						if(x0 > y0) x = x0;
+						else z = y0;
+					}
+					
+					if(y == z && y1 == z1) {
+					
+						if(y0 > z0) y = y0;
+						else z = z0; 
+					}
+				}
+				
+				if(state[x * n + x1][y * n + y1][z * n + z1] == -1) {
+				
+					state[x * n + x1][y * n + y1][z * n + z1] = 1 + state[x0 * n + x1][y0 * n + y1][z0 * n + z1];
+					Q.push({x, x1, y, y1, z, z1});
+				}
+			}
+			
+			{
+				int x = x1, y = y1, z = z1;
+				
+				if(x1 < n - 1 && grid[x0][x1 + 1] != '#') ++x;
+				if(y1 < n - 1 && grid[y0][y1 + 1] != '#') ++y;
+				if(z1 < n - 1 && grid[z0][z1 + 1] != '#') ++z;
+			
+				for(int k = 0; k < 2; ++k) {
+					
+					if(x == z && x0 == z0) {
+						
+						if(x1 < z1) x = x1;
+						else z = z1;
+					}
+						
+					if(x == y && x0 == y0) {
+						
+						if(x1 < y1) x = x1;
+						else z = y1;
+					}
+						
+					if(y == z && y0 == z0) {
+						
+						if(y1 < z1) y = y1;
+						else z = z1;
+					}
+				}
+				
+				if(state[x0 * n + x][y0 * n + y][z0 * n + z] == -1) {
+				
+					state[x0 * n + x][y0 * n + y][z0 * n + z] = 1 + state[x0 * n + x1][y0 * n + y1][z0 * n + z1];
+					Q.push({x0, x, y0, y, z0, z});
+				}	
+			}
+			
+			{
+				int x = x1, y = y1, z = z1;
+				
+				if(x1 && grid[x0][x1 - 1] != '#') --x;
+				if(y1 && grid[y0][y1 - 1] != '#') --y;
+				if(z1 && grid[z0][z1 - 1] != '#') --z;
+				
+				for(int k = 0; k < 2; ++k) {
+					
+					if(x == z && x0 == z0) {
+						
+						if(x1 > z1) x = x1;
+						else z = z1;
+					}
+						
+					if(x == y && x0 == y0) {
+						
+						if(x1 > y1) x = x1;
+						else z = y1;
+					}
+						
+					if(y == z && y0 == z0) {
+						
+						if(y1 > z1) y = y1;
+						else z = z1;
+					}
+				}
+				
+				if(state[x0 * n + x][y0 * n + y][z0 * n + z] == -1) {
+				
+					state[x0 * n + x][y0 * n + y][z0 * n + z] = 1 + state[x0 * n + x1][y0 * n + y1][z0 * n + z1];
+					Q.push({x0, x, y0, y, z0, z});
+				}
+			}
+		}
+		
+		x0 = target[0][0];
+		x1 = target[0][1];
+		
+		y0 = target[1][0];
+		y1 = target[1][1];
+		
+		z0 = target[2][0];
+		z1 = target[2][1];
+		
+		int min_cst = INT_MAX;
+		
+		if(state[x0 * n + x1][y0 * n + y1][z0 * n + z1] != -1) min_cst = state[x0 * n + x1][y0 * n + y1][z0 * n + z1];
+		if(state[x0 * n + x1][z0 * n + z1][y0 * n + y1] != -1) min_cst = min(min_cst, state[x0 * n + x1][z0 * n + z1][y0 * n + y1]);
+		if(state[y0 * n + y1][z0 * n + z1][x0 * n + x1] != -1) min_cst = min(min_cst, state[y0 * n + y1][z0 * n + z1][x0 * n + x1]);
+		if(state[y0 * n + y1][x0 * n + x1][z0 * n + z1] != -1) min_cst = min(min_cst, state[y0 * n + y1][x0 * n + x1][z0 * n + z1]);
+		if(state[z0 * n + z1][x0 * n + x1][y0 * n + y1] != -1) min_cst = min(min_cst, state[z0 * n + z1][x0 * n + x1][y0 * n + y1]);
+		if(state[z0 * n + z1][y0 * n + y1][x0 * n + x1] != -1) min_cst = min(min_cst, state[z0 * n + z1][y0 * n + y1][x0 * n + x1]);
+		
+		printf("Case %d: ", i);
+		
+		if(min_cst == INT_MAX)
+			puts("trapped");
+		else
+			printf("%d\n", min_cst);
+			
+		for(int i = 0; i < n; ++i) 
+			delete[] grid[i];
+		
+		for(int i = 0; i < N; ++i) {
+		
+			for(int j = 0; j < N; ++j)
+				delete[] state[i][j];
+				
+			delete[] state[i];
+		}
+		
+		delete[] grid;
+		delete[] state;
+	}
+	
+	return 0;
 }
