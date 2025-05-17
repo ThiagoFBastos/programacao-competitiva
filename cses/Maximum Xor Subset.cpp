5824a1577c3c05f6a73886e83a421b091b24123a
Thiago Bastos <thiagobastos1705@gmail.com>
2025-05-17 08:08:57 -0300

    add: Maximum Xor Subset

---
+++ cses/Maximum Xor Subset.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+constexpr int N = 30;
+ 
+vector<int> gauss[N];
+ 
+int msb(int x) {
+	if(!x) return 0;
+	return 31 - __builtin_clz(x);
+}
+ 
+int main() {
+ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+ 
+	int n, ans {};
+ 
+	cin >> n;
+ 
+	for(int i = 0; i < n; ++i) {
+		int x;
+ 
+		cin >> x;
+ 
+		if(x) 
+			gauss[msb(x)].push_back(x);
+	}
+ 
+	for(int i = N - 1; i >= 0; --i) {
+		auto& S = gauss[i];
+ 
+		if(S.empty())
+			continue;
+ 
+		int pivot = S[0];
+ 
+		if(ans < (ans ^ pivot))
+			ans ^= pivot;
+ 
+		for(int x : S) {
+			int y = x ^ pivot;
+ 
+			if(y)
+				gauss[msb(y)].push_back(y);
+		}
+	}
+ 
+	cout << ans << '\n';
+ 
+	return 0;
+}
