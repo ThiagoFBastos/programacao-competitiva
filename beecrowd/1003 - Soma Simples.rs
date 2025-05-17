a7cf074304fc796a93da486f83a308b95dec1888
Thiago Bastos <thiagobastos1705@gmail.com>
2025-05-17 19:42:41 -0300

    add: 1003 - Soma Simples

---
+++ beecrowd/1003 - Soma Simples.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn main() {
+
+    let mut first_line = String::new();
+    let mut second_line = String::new();
+
+    io::stdin()
+        .read_line(&mut first_line)
+        .expect("Failed to read a line");
+
+    io::stdin()
+        .read_line(&mut second_line)
+        .expect("Failed to read a line");
+
+    let a: i32 = first_line.trim()
+                    .parse()
+                    .expect("Failed to parse a integer value");
+
+    let b: i32 = second_line.trim()
+                    .parse()
+                    .expect("Failed to parse a integer value");
+
+    println!("SOMA = {}", a + b);
+}
