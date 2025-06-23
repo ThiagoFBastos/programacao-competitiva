f04b8320c48a684b77fd79ed089297a03c377ea6
Thiago Bastos <thiagobastos1705@gmail.com>
2025-06-23 19:42:08 -0300

    add: 1805 - Soma Natural

---
+++ beecrowd/1805 - Soma Natural.rs
@@ -0,0 +1,72 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::cmp::*;
+use std::collections::*;
+
+struct Scanner {
+   buffer : VecDeque<String>,
+   eof: bool
+}
+ 
+impl Scanner {
+ 
+   fn new() -> Scanner {
+      Scanner {
+         buffer: VecDeque::new(),
+         eof: false
+      }
+   }
+   
+   fn next<T : std::str::FromStr + Default >(&mut self) -> T {
+ 
+      if self.buffer.len() == 0 {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => self.eof = true,
+            Ok(_) => {},
+            Err(e) => panic!("panico {}", e)
+         };
+
+         for word in input.split_whitespace() {
+            self.buffer.push_back(word.to_string())
+         }
+      }
+
+      if self.eof {
+         return T::default();
+      }
+ 
+      let front = self.buffer.pop_front().unwrap();
+      
+      front.parse::<T>().ok().unwrap()
+   }
+
+   fn read_line(&mut self) -> String {
+      let mut input = String::new();
+         
+      match std::io::stdin().read_line(&mut input) {
+         Ok(0) => self.eof = true,
+         Ok(_) => {},
+         Err(e) => panic!("{}", e)
+      };
+
+      return input;
+   }
+
+   fn is_eof(&self) -> bool {
+      self.eof
+   }
+}
+
+fn main() {
+
+   let mut sc = Scanner::new();
+   
+   let a = sc.next::<i64>();
+   let b = sc.next::<i64>();
+
+   let sum = |l: i64, r: i64| (l + r) * (r - l + 1) / 2;
+
+   println!("{}", sum(a, b));
+}
