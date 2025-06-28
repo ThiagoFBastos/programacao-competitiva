e53d9f3ab30ed6fb0bd3547d15948430fd0805a8
Thiago Bastos <thiagobastos1705@gmail.com>
2025-06-28 13:10:08 -0300

    add: 1241 - Encaixa ou Não II

---
+++ beecrowd/1241 - Encaixa ou Não II.rs
@@ -0,0 +1,85 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::cmp::*;
+use std::collections::*;
+
+struct Scanner {
+   buffer : VecDeque<String>,
+   eof: bool
+}
+ 
+impl Scanner {
+ 
+   fn new() -> Scanner {
+      Scanner {
+         buffer: VecDeque::new(),
+         eof: false
+      }
+   }
+   
+   fn next<T : std::str::FromStr + Default >(&mut self) -> T {
+ 
+      if self.buffer.len() == 0 {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => self.eof = true,
+            Ok(_) => {},
+            Err(e) => panic!("panico {}", e)
+         };
+
+         for word in input.split_whitespace() {
+            self.buffer.push_back(word.to_string())
+         }
+      }
+
+      if self.eof {
+         return T::default();
+      }
+ 
+      let front = self.buffer.pop_front().unwrap();
+      
+      front.parse::<T>().ok().unwrap()
+   }
+
+   fn read_line(&mut self) -> String {
+      let mut input = String::new();
+         
+      match std::io::stdin().read_line(&mut input) {
+         Ok(0) => self.eof = true,
+         Ok(_) => {},
+         Err(e) => panic!("{}", e)
+      };
+
+      if input.ends_with('\n') {
+         input.pop();
+         if input.ends_with('\r') {
+            input.pop();
+         }
+      }
+
+      return input;
+   }
+
+   fn is_eof(&self) -> bool {
+      self.eof
+   }
+}
+
+fn main() {
+
+   let mut sc = Scanner::new();
+
+   let cases = sc.next::<usize>();
+
+   for _i in 0..cases {
+      let a = sc.next::<String>();
+      let b = sc.next::<String>();
+
+      if a.ends_with(&b) {
+         println!("encaixa");
+      } else {
+         println!("nao encaixa");
+      }
+   }
+}
