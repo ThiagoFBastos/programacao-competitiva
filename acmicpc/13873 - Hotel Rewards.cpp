9957453b534d9f84186334d349c522a2bcf24032
Thiago Bastos <thiagobastos1705@gmail.com>
2025-07-29 19:51:05 -0300

    add: Hotel Rewards

---
+++ acmicpc/13873 - Hotel Rewards.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define INF 1000000000
+#define INFLL 1000000000000000000ll
+#define EPS 1e-9
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define pb push_back
+#define fi first
+#define sc second
+
+using i64 = long long;
+using u64 = unsigned long long;
+using ld = long double;
+using ii = pair<int, int>;
+using i128 = __int128;
+
+void solve() {
+	int n, k;
+	
+	cin >> n >> k;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+	i64 s = 0;
+
+	for(int i = 0, j = 0; i < n; ++i) {
+		int a;
+		cin >> a;
+		s += a;
+		if(j < k) {
+			if(pq.empty() || pq.top() >= a) ++j;
+			else {	
+				++j, s -= a - pq.top();
+				pq.pop();
+				pq.push(a);
+			}
+		} else if(j >= k) {
+			j -= k, s -= a;	
+			pq.push(a);
+		}
+	}
+
+	cout << s << '\n';
+}
+
+int main() {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while(t--) solve();
+	return 0;
+}
