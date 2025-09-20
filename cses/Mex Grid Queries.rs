4cadba1c2c36f6e39ada7ee325197f6a1e30a9c9
Thiago Bastos <thiagobastos1705@gmail.com>
2025-09-20 19:39:47 -0300

    add: Mex Grid Queries

---
+++ cses/Mex Grid Queries.rs
@@ -0,0 +1,53 @@
+#![allow(unused_imports)]
+use std::collections::{VecDeque};
+use std::io::{self, Error, ErrorKind, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new()
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of Line"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+      }
+ 
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn main() {
+   let stdout = std::io::stdout();
+   let mut handle = std::io::BufWriter::new(stdout);
+ 
+   let mut sc = Scanner::new();
+   
+   let y = sc.next::<i32>().ok().unwrap() - 1;
+   let x = sc.next::<i32>().ok().unwrap() - 1;
+
+   writeln!(handle, "{}", x ^ y).ok();
+}
