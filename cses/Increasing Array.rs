8c5805e3fc43b254bda5091fa3cefeecc71206c1
Thiago Bastos <thiagobastos1705@gmail.com>
2025-09-21 14:38:45 -0300

    add: Increasing Array

---
+++ cses/Increasing Array.rs
@@ -0,0 +1,63 @@
+#![allow(unused_imports)]
+use std::collections::{VecDeque};
+use std::io::{self, Error, ErrorKind, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new()
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of Line"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+      }
+ 
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn main() {
+   let stdout = std::io::stdout();
+   let mut handle = std::io::BufWriter::new(stdout);
+ 
+   let mut sc = Scanner::new();
+   
+   let n = sc.next::<usize>().ok().unwrap();
+
+   let mut arr = (0..n).map(|_| sc.next::<i32>().ok().unwrap()).collect::<Vec<_>>();
+
+   let mut operations = 0;
+
+   for i in 1..n {
+      if arr[i] < arr[i - 1] {
+         operations += (arr[i - 1] - arr[i]) as i64;
+         arr[i] = arr[i - 1];
+      }
+   }
+
+   writeln!(handle, "{}", operations).ok();
+}
