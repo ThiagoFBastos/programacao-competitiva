8db2a6372c93890a1b4568d53486be9451818b3e
Thiago Bastos <thiagobastos1705@gmail.com>
2025-09-27 20:09:40 -0300

    add: Collecting Numbers

---
+++ cses/Collecting Numbers.rs
@@ -0,0 +1,65 @@
+#![allow(unused_imports)]
+use std::collections::{VecDeque};
+use std::io::{self, Error, ErrorKind, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new()
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of Line"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+      }
+ 
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn main() {
+   let stdout = std::io::stdout();
+   let mut handle = std::io::BufWriter::new(stdout);
+ 
+   let mut sc = Scanner::new();
+
+   let n = sc.next::<usize>().ok().unwrap();
+
+   let mut pos = vec![0; n + 1];
+
+   (0..n).for_each(|i| {
+      let value = sc.next::<usize>().ok().unwrap();
+      pos[value] = i;
+   });
+
+   let mut count = 1;
+
+   for i in 2..=n {
+      count += if pos[i] < pos[i - 1] { 1 } else { 0 };
+   }
+
+   writeln!(handle, "{count}").ok();
+ }
