e9127efcba6f765c7726121f55031ce44216e04f
Thiago Bastos <thiagobastos1705@gmail.com>
2025-09-28 13:38:43 -0300

    add: Josephus Problem I

---
+++ cses/Josephus Problem I.rs
@@ -0,0 +1,84 @@
+#![allow(unused_imports)]
+use std::collections::{VecDeque};
+use std::io::{self, Error, ErrorKind, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new()
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of Line"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+      }
+ 
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn main() {
+   let stdout = std::io::stdout();
+   let mut handle = std::io::BufWriter::new(stdout);
+ 
+   let mut sc = Scanner::new();
+
+   let n = sc.next::<i32>().ok().unwrap();
+
+   let mut a = 1;
+   let mut b = 1;
+   let mut is_first = false;
+   let mut k = n;
+
+   while k > 0 {
+      let mut j = 0;
+
+      if is_first {
+
+         while a + 2 * b * j <= n {
+            write!(handle, "{} ", a + 2 * b * j).ok();
+            j += 1;
+         }
+
+         is_first = k % 2 == 0;
+         a += b;
+         k /= 2;
+      } else {
+         
+         while a + b + 2 * b * j <= n {
+            write!(handle, "{} ", a + b + 2 * b * j).ok();
+            j += 1;
+         }
+
+         is_first = k % 2 != 0;
+			k = (k + 1) / 2;
+      }
+
+      b <<= 1;
+   }
+
+   write!(handle, "\n").ok();
+ }
