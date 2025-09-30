5cca60d5e6a233077b5e976d19a03d82a2cea728
Thiago Bastos <thiagobastos1705@gmail.com>
2025-09-30 19:51:28 -0300

    add: Removing Digits

---
+++ cses/Removing Digits.rs
@@ -0,0 +1,70 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::collections::{VecDeque};
+use std::io::{self, Error, ErrorKind, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new()
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of File"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+      }
+ 
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn max_digit(mut num: i32) -> i32 {
+   let mut digit = 0;
+
+   while num > 0 {
+      digit = digit.max(num % 10);
+      num /= 10;
+   }
+
+   digit
+}
+
+fn main() {
+   let stdout = std::io::stdout();
+   let mut handle = std::io::BufWriter::new(stdout);
+ 
+   let mut sc = Scanner::new();
+
+   let n = sc.next::<usize>().ok().unwrap();
+
+   let mut dp = vec![0; n + 1];
+
+   for i in 1..=n {
+      dp[i] = 1 + dp[i - max_digit(i as i32) as usize];
+   }
+
+   writeln!(handle, "{}", dp[n]).ok();
+ }
