e449312763bb9ed75746266bd64705e89f78c108
Thiago Bastos <40869714+ThiagoFBastos@users.noreply.github.com>
2025-10-01 12:35:59 -0300

    add: Many A + B

---
+++ yosupo/Many A + B.rs
@@ -0,0 +1,58 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::collections::{VecDeque, BTreeMap};
+use std::io::{self, Error, ErrorKind, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new()
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of File"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+      }
+ 
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn main() {
+   let stdout = std::io::stdout();
+   let mut handle = std::io::BufWriter::new(stdout);
+ 
+   let mut sc = Scanner::new();
+
+   let t = sc.next::<usize>().ok().unwrap();
+
+   for _ in 0..t {
+      let a = sc.next::<u64>().ok().unwrap();
+      let b = sc.next::<u64>().ok().unwrap();
+
+      writeln!(handle, "{}", a + b).ok();
+   }
+ }
