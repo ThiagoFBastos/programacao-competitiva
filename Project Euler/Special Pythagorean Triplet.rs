536ab818d9eac2b352286dde36928f45aeedb152
Thiago Bastos <40869714+ThiagoFBastos@users.noreply.github.com>
2025-11-15 20:38:40 -0300

    add: Special Pythagorean Triplet

---
+++ Project Euler/Special Pythagorean Triplet.rs
@@ -0,0 +1,72 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::collections::{VecDeque};
+use std::io::{self, Error, ErrorKind, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new()
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of File"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+      }
+ 
+      if self.buffer.is_empty() {
+         self.buffer.push_back("".to_string());
+      }
+
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn main() {
+   let stdout = std::io::stdout();
+   let mut handle = std::io::BufWriter::new(stdout);
+ 
+   const N: i32 = 1_000;
+
+   for a in 0..N {
+      for b in a + 1..N {
+         if a + b > N {
+            break;
+         }
+
+         let c = N - a - b;
+
+         if b >= c {
+            break;
+         } else if a.pow(2) + b.pow(2) == c.pow(2) {
+            writeln!(handle, "{}", a * b * c).ok();
+            return;
+         }
+      }
+   }
+
+   panic!();
+}
