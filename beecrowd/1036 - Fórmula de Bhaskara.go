3562b33bf8322640b7d07e8e29f6c975a8080ca9
Thiago Bastos <40869714+ThiagoFBastos@users.noreply.github.com>
2025-12-05 19:33:25 -0300

    add:  1036 - Fórmula de Bhaskara

---
+++ beecrowd/1036 - Fórmula de Bhaskara.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, b, c float64
+
+	fmt.Scan(&a, &b, &c)
+
+	delta := math.Pow(b, 2) - 4*a*c
+
+	if delta < 0 || a == 0 {
+		fmt.Println("Impossivel calcular")
+		return
+	}
+
+	x1 := (-b + math.Sqrt(delta)) / (2 * a)
+	x2 := (-b - math.Sqrt(delta)) / (2 * a)
+
+	fmt.Printf("R1 = %.5f\n", x1)
+	fmt.Printf("R2 = %.5f\n", x2)
+}
