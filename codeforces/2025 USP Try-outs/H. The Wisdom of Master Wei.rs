6a5e33d31b3980cc62bf360e41849924e149c16d
Thiago Bastos <40869714+ThiagoFBastos@users.noreply.github.com>
2025-12-18 12:39:47 -0300

    add: H. The Wisdom of Master Wei

---
+++ codeforces/2025 USP Try-outs/H. The Wisdom of Master Wei.rs
@@ -0,0 +1,61 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::collections::{VecDeque, HashMap};
+use std::{i32, vec};
+use std::io::{self, Error, ErrorKind, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new()
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+         
+         match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of File"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+      }
+ 
+      if self.buffer.is_empty() {
+         self.buffer.push_back("".to_string());
+      }
+
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn main() {
+   let stdout = std::io::stdout();
+   let mut handle = std::io::BufWriter::new(stdout);
+
+   let mut sc = Scanner::new();
+
+   let w = sc.next::<i32>().unwrap();
+   let k = sc.next::<i32>().unwrap();
+
+   let x = 2 * k - w;
+
+   writeln!(handle, "{}", x).ok();
+}
