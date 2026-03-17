0788f14f1f415d23937420787580c0820e505455
Thiago Felipe Bastos da Silva <thiagobastos1705@gmail.com>
2026-03-17 20:10:42 -0300

    add: 1007 - Diferença

---
+++ beecrowd/1007 - Diferença.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+	val a = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
+    val c = readLine()!!.toInt()
+    val d = readLine()!!.toInt()
+
+    val product = a * b - c * d
+
+    println("DIFERENCA = $product")
+}
