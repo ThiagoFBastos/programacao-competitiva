c04625a623276e2492a26a5c50a88b9042ad73ef
Thiago Felipe Bastos da Silva <thiagobastos1705@gmail.com>
2026-05-04 19:31:14 -0300

    add: 3488 - Binary/Hexadecimal Number Systems

---
+++ beecrowd/3488 - Binary Hexadecimal Number Systems.rs
@@ -0,0 +1,66 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use std::collections::{VecDeque};
+use std::io::{self, BufRead, Error, ErrorKind, Read, Write};
+
+struct Scanner {
+   buffer: VecDeque<String>,
+   reader: io::BufReader<io::Stdin>
+}
+
+impl Scanner {
+ 
+   fn new() -> Self {
+      Self {
+         buffer: VecDeque::new(),
+         reader: io::BufReader::new(io::stdin())
+      }
+   }
+   
+   fn next<T: std::str::FromStr>(&mut self) -> io::Result<T> {
+ 
+      if self.buffer.is_empty() {
+         let mut input = String::new();
+
+         match self.reader.read_line(&mut input) {
+            Ok(0) => {
+               return Err(Error::new(ErrorKind::UnexpectedEof, "End Of File"));
+            } Ok(_) => {
+               
+            } Err(e) => {
+               return Err(e);
+            }
+         }
+ 
+         self.buffer = input.split_whitespace()
+                            .map(|x| x.to_string())
+                            .collect();
+
+         if self.buffer.is_empty() {
+            self.buffer.push_back("".to_string());
+         }
+      }
+
+      let front = self.buffer.pop_front().unwrap();
+      
+      Ok(front.parse::<T>().ok().unwrap())
+   }
+}
+
+fn is_power_of_two(n: i32) -> bool {
+   if n <= 0 {
+      return false;
+   }
+
+   n & (n - 1) == 0
+}
+
+fn main() {
+   let mut writer = io::BufWriter::new(io::stdout());
+
+   let mut sc = Scanner::new();
+
+   let n = sc.next::<i32>().unwrap();
+
+   writeln!(writer, "{}", is_power_of_two(n)).ok();
+}
