230a92b3c8e0c5078dfbacb71b58a48afe6c5b81
Thiago Felipe Bastos da Silva <thiagobastos1705@gmail.com>
2026-06-07 19:58:20 -0300

    update: 1462 - Popularidade no Facebook

--- beecrowd/1462 - Popularidade no Facebook.cpp
+++ beecrowd/1462 - Popularidade no Facebook.cpp
@@ -2,49 +2,58 @@
 
 using namespace std;
 
+bool erdos_gallai(std::vector<int> degrees)
+{
+    const auto n = degrees.size();
+
+    std::sort(degrees.rbegin(), degrees.rend());
+
+    std::stack<int> st;
+    long long left_sum = accumulate(degrees.begin(), degrees.end(), 0ll);
+    long long right_sum {}, cnt {};
+
+    bool is_possible = left_sum % 2 == 0;
+
+    for(std::size_t i = n; i > 0; --i)
+    {
+        if(!is_possible)
+            break;
+
+        while(!st.empty() && st.top() >= static_cast<int>(i))
+        {
+            auto val = st.top();
+            st.pop();
+            right_sum -= val;
+            ++cnt;
+        }
+
+        is_possible = is_possible && left_sum <= static_cast<long long>(i) * static_cast<long long>(i - 1) + right_sum + cnt * static_cast<long long>(i);
+        
+        st.push(degrees[i - 1]);
+        left_sum -= degrees[i - 1];
+        right_sum += degrees[i - 1];
+    }
+
+    return is_possible;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
+    size_t n;
 
     while(cin >> n) {
-        auto view = views::iota(0, n) | views::transform([](auto) {
-            int deg;
-            cin >> deg;
-            return deg;
-        });
+        vector<int> degrees;
 
-        vector<int> deg;
+        auto input = views::iota(size_t {}, n) | views::transform([](auto) { int deg; cin >> deg; return deg; });
 
-        ranges::copy(view, back_inserter(deg));
+        ranges::copy(input, back_inserter(degrees));
 
-        sort(deg.rbegin(), deg.rend());
-
-        vector<long long> pre(n + 1, 0);
-        priority_queue<int> pq;
-        long long right_sum = 0, cnt {};
-
-        for(int i = 1; i <= n; ++i)
-            pre[i] = pre[i - 1] + deg[i - 1];
-
-        bool possible = pre[n] % 2 == 0;
-
-        for(int i : views::iota(1, n + 1) | views::reverse) {
-            while(!pq.empty() && pq.top() >= static_cast<int>(i)) {
-                auto val = pq.top();
-                pq.pop();
-                right_sum -= val;
-                ++cnt;
-            }
-
-            possible = possible && pre[i] <= i * (i - 1ll) + right_sum + cnt * i;
-            
-            pq.push(deg[i - 1]);
-            right_sum += deg[i - 1];
-        }
-
-        cout << (possible ? "possivel\n" : "impossivel\n");
+        if(erdos_gallai(move(degrees)))
+            cout << "possivel\n";
+        else
+            cout << "impossivel\n";
     }
 
     return 0;
