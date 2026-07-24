fc9a6af42212b1cf65b0676394eaa6b63d22ed25
Thiago Bastos <thiagobastos1705@gmail.com>
2026-07-23 21:03:14 -0300

    add: 2983 - Nem Tudo é Greve

---
+++ beecrowd/2983 - Nem Tudo é Greve.cpp
@@ -0,0 +1,255 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+namespace math
+{
+
+/**
+ * @brief Compute the greatest common divisor of two numbers
+ * @param a a number
+ * @param b a number
+ * @return the greatest common divisor of a and b
+ * @note This function is necessary because std::gcd does not support __int128
+ * numbers, which are used in the factorization process.
+ */
+inline __int128 gcd(__int128 a, __int128 b)
+{
+	while (b != 0)
+	{
+		auto temp = b;
+		b = a % b;
+		a = temp;
+	}
+
+	return a;
+}
+
+/**
+ * @brief Compute the absolute value of a number
+ * @param x a number
+ * @return the absolute value of x
+ * @note This function is necessary because std::abs does not support __int128
+ * numbers, which are used in the factorization process.
+ */
+inline __int128 abs(__int128 x)
+{
+	return x < 0 ? -x : x;
+}
+
+/**
+ * @brief Take the modulo of the mutiplication of two numbers
+ * @param a a number
+ * @param b a number
+ * @param m the modulo
+ */
+template<typename T>
+inline T modMul(T a, T b, T m)
+{
+	if constexpr(std::is_same_v<T, long long> || std::is_same_v<T, __int128>)
+	{
+		return static_cast<T>(static_cast<__int128>(a) * b % m);
+	}
+	else
+	{
+		return static_cast<T>(static_cast<long long>(a) * b % m);
+	}
+}
+ 
+/**
+ * @brief Modular binary exponentiation
+ * @param a the base number
+ * @param p the power
+ * @param m the modulo
+ */
+template<typename T>
+inline T modMulExp(T a, T p, T m)
+{
+	T ans = 1;
+	for(; p > 0; p >>= 1) 
+	{
+		if(p & 1) ans = modMul(ans, a, m);
+		a = modMul(a, a, m);
+	}
+	return ans;
+}
+ 
+/**
+ * @brief Miller-Rabin algorithm to check whether a number is prime
+ * @param n the target number
+ * @return true if the number is prime, and false otherwise
+ */
+template<typename T>
+inline bool miller(T n)
+{
+	const int pn = 9;
+	const T p[] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+
+	for(T k : p)
+		if(n % k == 0)
+			return n == k;
+	
+	if(n < p[pn - 1]) return false;
+
+	T t = n - 1;
+	int s = 0;
+
+	while(~t & 1) t >>= 1, ++s;
+
+	for(T k : p)
+	{
+		T pt = modMulExp(k, t, n);
+
+		if(pt == 1) continue;
+
+		bool ok = false;
+
+		for(int j = 0; j < s && !ok; ++j)
+		{
+			ok = ok || pt == n - 1;
+			pt = modMul(pt, pt, n);
+		}
+
+		if(!ok) return false;
+	}
+
+	return true;
+}
+ 
+/**
+ * @brief Auxiliary function used by the rho method
+ * @param x the number to square up
+ * @param c the linear coefficient
+ * @param mod the number to take the modulo of the function
+ * @return (x^2 + c) % mod
+ */
+template<typename T>
+inline T f(T x, T c, T mod)
+{
+	return (modMul(x, x, mod) + c) % mod;
+}
+ 
+/**
+ * @brief Pollard-Rho algorithm to find a factor of n
+ * @param n the target number
+ * @return a factor of n != 1, n (cannot be called for prime numbers)
+ */
+template<typename T>
+inline T rho(T n)
+{
+	std::mt19937 rnd(std::chrono::system_clock::now().time_since_epoch().count());
+	T x, y, g = n;
+
+	while(g == n)
+	{
+		T x0 = rnd(), c = rnd();
+		x = y = x0;
+		g = 1;
+
+		while(g == 1)
+		{
+			x = f(x, c, n);
+			y = f(f(y, c, n), c, n);
+
+			if constexpr(std::is_same_v<decltype(n), __int128>)
+			{
+				g = gcd(abs(x - y), n);
+			}
+			else
+			{
+				g = std::gcd(std::abs(x - y), n);
+			}
+		}
+	}
+
+	return g;
+}
+	
+/**
+ * @brief Factor a number using the pollard-rho algorithm and a brute force of
+ * prime numbers until n^(1 / 4)
+ * @param n the target number
+ * @return a vector of factors as (p, k), ie, p^k
+ */
+template<typename T>
+inline auto factorize(T n)
+{	
+	std::vector<std::pair<T, int>> f;
+	
+	if(~n & 1)
+	{
+		int k = 0;
+
+		while(~n & 1)
+		{
+			n >>= 1;
+			++k;
+		}
+
+		f.emplace_back(2, k);
+	}
+
+	for(T p = 3; p * p * p * p <= n; p += 2)
+	{
+		if(n % p) continue;
+
+		int i = 0;
+		while(n % p == 0) n /= p, ++i;
+		f.emplace_back(p, i);
+	}
+
+	while(n != 1)
+	{
+		if(miller(n))
+		{
+			f.emplace_back(n, 1);
+			break;
+		}
+		
+		T k = rho(n);
+		
+		if(!miller(k))
+			k = rho(k);
+
+		int i = 0;
+		while(n % k == 0) n /= k, ++i;
+		f.emplace_back(k, i);
+	}
+
+	return f;
+}
+
+}
+
+int main() {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(0);
+
+	size_t n;
+
+    cin >> n;
+
+    vector<int> primes(n);
+
+    for(int& value : primes)
+        cin >> value;
+    
+    sort(primes.begin(), primes.end());
+    primes.resize(unique(primes.begin(), primes.end()) - primes.begin());
+
+    auto it = remove_if(primes.begin(), primes.end(), [](int value) { return !value || !math::miller(value);});
+
+    primes.erase(it, primes.end());
+    
+    cout << primes.size() << '\n';
+
+    for(size_t i = 0; i < primes.size(); ++i) {
+        cout << primes[i];
+        cout << (i + 1 == primes.size() ? "." : ", ");
+    }
+
+    cout << '\n';
+
+	return 0;
+}
+ 
