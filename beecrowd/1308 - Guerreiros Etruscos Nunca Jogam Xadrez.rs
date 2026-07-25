0de3e4ad819e956fda50f1cb3a1b9858fa4cec39
Thiago Bastos <thiagobastos1705@gmail.com>
2026-07-25 15:43:42 -0300

    add: 1308 - Guerreiros Etruscos Nunca Jogam Xadrez | Rust version

---
+++ beecrowd/1308 - Guerreiros Etruscos Nunca Jogam Xadrez.rs
@@ -0,0 +1,69 @@
+#![allow(dead_code)]
+use std::{
+    io::{self, Read, Write},
+    writeln,
+};
+
+struct Scanner {
+    buffer: Vec<u8>,
+    index: usize,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut input = Vec::new();
+
+        io::stdin().read_to_end(&mut input).unwrap();
+
+        Self {
+            buffer: input,
+            index: 0,
+        }
+    }
+
+    fn has_next(&mut self) -> bool {
+        while self.index < self.buffer.len() && self.buffer[self.index].is_ascii_whitespace() {
+            self.index += 1;
+        }
+
+        self.index < self.buffer.len()
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        if !self.has_next() {
+            panic!("End Of File");
+        }
+
+        let start = self.index;
+
+        while self.index < self.buffer.len() && !self.buffer[self.index].is_ascii_whitespace() {
+            self.index += 1;
+        }
+
+        std::str::from_utf8(&self.buffer[start..self.index])
+            .unwrap()
+            .parse::<T>()
+            .ok()
+            .unwrap()
+    }
+}
+
+fn main() {
+    let mut writer = io::BufWriter::with_capacity(1 << 20, io::stdout());
+
+    let mut sc = Scanner::new();
+
+    let t = sc.next::<usize>();
+
+    for _ in 0..t {
+        let n = sc.next::<i64>();
+
+        let mut k = (2.0 * n as f64).sqrt() as i64;
+
+        if k * (k + 1) / 2 > n {
+            k -= 1;
+        }
+
+        writeln!(writer, "{}", k).ok();
+    }
+}
