8ef5c338ab14312514959143841014bd5830a0ca
Thiago Felipe Bastos da Silva <thiagobastos1705@gmail.com>
2026-07-27 19:25:50 -0300

    add: 2829 - Maiúsculas, Por Quê? | Rust version

---
+++ beecrowd/2829 - Maiúsculas, Por Quê.rs
@@ -0,0 +1,67 @@
+#![allow(dead_code)]
+use std::io::{self, Read, Write};
+
+struct Scanner {
+    buffer: Vec<u8>,
+    index: usize,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut input = Vec::new();
+
+        io::stdin().read_to_end(&mut input).unwrap();
+
+        Self {
+            buffer: input,
+            index: 0,
+        }
+    }
+
+    fn has_next(&mut self) -> bool {
+        while self.index < self.buffer.len() && self.buffer[self.index].is_ascii_whitespace() {
+            self.index += 1;
+        }
+
+        self.index < self.buffer.len()
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        if !self.has_next() {
+            panic!("End Of File");
+        }
+
+        let start = self.index;
+
+        while self.index < self.buffer.len() && !self.buffer[self.index].is_ascii_whitespace() {
+            self.index += 1;
+        }
+
+        std::str::from_utf8(&self.buffer[start..self.index])
+            .unwrap()
+            .parse::<T>()
+            .ok()
+            .unwrap()
+    }
+}
+
+fn main() {
+    let mut writer = io::BufWriter::with_capacity(1 << 20, io::stdout());
+
+    let mut sc = Scanner::new();
+
+    let n = sc.next::<usize>();
+
+    let mut str_pairs = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        let s = sc.next::<String>();
+        str_pairs.push((s.to_lowercase(), s));
+    }
+
+    str_pairs.sort();
+
+    for (_, y) in str_pairs {
+        writeln!(writer, "{}", y).ok();
+    }
+}
