806b8c915a95fa704b2f6b03bf9f850fa246d6dc
Thiago Felipe Bastos da Silva <thiagobastos1705@gmail.com>
2026-09-07 14:08:21 -0300

    add: 1773 - Preso no Castelo

---
+++ beecrowd/1773 - Preso no Castelo.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m;
+	
+	while(cin >> n >> m) {
+		vector<vector<int>> adj(n), keyLocation(n);
+
+		for(int i = 0; i < m; ++i) {
+			int u, v;
+			cin >> u >> v;
+			--u, --v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+
+		for(int i = 1; i < n; ++i) {
+			int loc;
+			cin >> loc;
+			keyLocation[loc - 1].push_back(i);
+		}
+
+		vector<bool> vis(n, false);
+		unordered_set<int> border, keys;
+		queue<int> q;
+		
+		border.reserve(n);
+		keys.reserve(n);
+
+		/*
+			1. vértice contém chave para outro vértice (não visitado) 
+				1.1 - se está na borda
+					- coloca esses vértices da fila
+				1.2 - se não está na borda
+					- guarda a chave
+			2. vértice é adjacente a um vértice na borda não visitado
+				- se já contém chave para ele coloca na fila
+			3. resto dos vértices não visitados
+				- coloca na borda 
+		*/
+
+		vis[0] = true;
+		q.push(0);
+
+		while(!q.empty()) {
+			int u = q.front();
+
+			q.pop();
+
+			for(int v : keyLocation[u]) {
+				keys.insert(v);
+
+				if(!vis[v] && border.count(v)) {
+					vis[v] = true;
+					q.push(v);
+				}
+			}
+
+			for(int v : adj[u]) {
+				if(vis[v])
+					continue;
+				else if(keys.count(v)) {
+					vis[v] = true;
+					q.push(v);
+				} else
+					border.insert(v);
+			}
+		}
+
+		if(ranges::all_of(vis, identity()))
+			cout << "sim\n";
+		else
+			cout << "nao\n";
+	}
+
+	return 0;
+}
